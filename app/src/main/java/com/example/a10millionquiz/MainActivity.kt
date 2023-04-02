38cd9d523f27c39271f0bdee2d299fbc247a2ba6
GrigoriyKostryukov <kostryukov-grigoriy@mail.ru>
2023-04-02 22:48:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a10millionquiz/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.a10millionquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a10millionquiz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/a10millionquiz/TitleFragment.kt
@@ -0,0 +1,28 @@
+package com.example.a10millionquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.example.a10millionquiz.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater, R.layout.fragment_title, container, false)
+
+        binding.playButton.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+        return binding.root
+    }
+
+
+}
